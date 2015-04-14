6162515a9d139fb348fb104ecb1eadffd5c7c3b3
Victor Ng <victor@crankycoder.com>
2015-04-14 15:47:08 -0400

    init checkin

---
+++ app/src/main/java/com/crankycoder/ndk1/AndroidNDK1SampleActivity.java
@@ -0,0 +1,56 @@
+package com.crankycoder.ndk1;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+
+public class AndroidNDK1SampleActivity extends ActionBarActivity {
+    static {
+        System.loadLibrary("ndk1");
+    }
+
+    private native void helloLog(String logThis);
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_android_ndk1_sample);
+
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                helloLog("This will log to LogCat via the native call.");
+            }
+        });
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_android_ndk1_sample, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
