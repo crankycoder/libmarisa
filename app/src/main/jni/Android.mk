2f26b8a5e1d2fa2e3b1088fb494664a11250da3f
Victor Ng <victor@crankycoder.com>
2015-04-16 11:41:39 -0400

    pulled in C++ and marisa code from python package

--- app/src/main/jni/Android.mk
+++ app/src/main/jni/Android.mk
@@ -1,10 +1,12 @@
+# A simple test for the minimal standard C++ library
+#
+
 LOCAL_PATH := $(call my-dir)
+LOCAL_CPP_FEATURES := exceptions rtti
 
 include $(CLEAR_VARS)
+LOCAL_MODULE := marisa
 
-LOCAL_LDLIBS := -llog
+LOCAL_SRC_FILES := test-libstl.cpp lib/marisa/agent.cc lib/marisa/keyset.cc lib/marisa/trie.cc lib/marisa/grimoire.cc lib/marisa/grimoire/io/mapper.cc lib/marisa/grimoire/io/reader.cc lib/marisa/grimoire/io/writer.cc lib/marisa/grimoire/trie/louds-trie.cc lib/marisa/grimoire/trie/tail.cc lib/marisa/grimoire/vector/bit-vector.cc
 
-LOCAL_MODULE    := ndk1
-LOCAL_SRC_FILES := native.c
-
-include $(BUILD_SHARED_LIBRARY)
+include $(BUILD_EXECUTABLE)
---
+++ app/src/main/jni/test-libstl.cpp
@@ -0,0 +1,9 @@
+#include <cerrno>
+#include <cstddef>
+#include <exception>
+
+int main(void)
+{
+    return 0;
+}
+
