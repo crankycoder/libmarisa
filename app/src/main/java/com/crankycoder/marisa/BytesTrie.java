52186e4c37d41933fb747755002c250f956f8593
Victor Ng <victor@crankycoder.com>
2015-04-25 00:15:32 -0400

    switched to method chaining

--- app/src/main/java/com/crankycoder/marisa/BytesTrie.java
+++ app/src/main/java/com/crankycoder/marisa/BytesTrie.java
@@ -29,7 +29,7 @@ public class BytesTrie extends Trie {
      Return a list of payloads (as byte objects) for a given key.
      */
     public List<byte[]> get(String key) {
-        return b_get_value(key.getBytes(Charset.forName("UTF-8")));
+        return get(key.getBytes(Charset.forName("UTF-8")));
     }
 
 
