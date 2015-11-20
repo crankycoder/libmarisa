c2b9116bbc16a47f7c67a80c7390c25317241e2c
Victor Ng <victor@crankycoder.com>
2015-11-20 11:07:37 -0500

    disabled dummy testcase in addon

--- libraries/marisa/addon/index.js
+++ libraries/marisa/addon/index.js
@@ -107,10 +107,10 @@ var page = pageMod.PageMod({
 
                                               this.rtrie_handle = push_trie(nDataBytes, dataPtr);
 
-                                              simple_test_trie = this.offlinegeo_mod.cwrap(
-                                                        'simple_test_trie', null, ['number']
-                                                      );
-                                              simple_test_trie(this.rtrie_handle);
+                                              ////////////////simple_test_trie = this.offlinegeo_mod.cwrap(
+                                              //          'simple_test_trie', null, ['number']
+                                              //        );
+                                              //simple_test_trie(this.rtrie_handle);
 
                                               // You must free the
                                               // memory after playing
