d7245fc2e054a76e49c4efcbfcba6c1f0a770cbd
Victor Ng <victor@crankycoder.com>
2016-02-03 17:20:48 -0500

    added a level of indirection for the trie locator

--- libraries/marisa/addon/index.js
+++ libraries/marisa/addon/index.js
@@ -5,26 +5,31 @@ var libofflinegeo = require("./lib/offlinegeo");
 var offlinegeo_mod = libofflinegeo.offline_factory();
 var libtrielookup = require("./lib/trielookup");
 
-var locator = new libtrielookup.TrieLocator(offlinegeo_mod);
+var locator_container = {};
+locator_container['locator'] = new libtrielookup.TrieLocator(offlinegeo_mod);
 
-function update_city_urls(locator) {
+function getLocator() {
+    return locator_container['locator'];
+}
+
+function update_city_urls() {
     var offline_base_url = simple_prefs.prefs["offlineCity"];
     var trie_url = offline_base_url + "/area.trie";
     var city_url = offline_base_url + "/ordered_city.csv";
 
     console.log("Updating with URLs: ["+trie_url+"]");
     console.log("Updating with URLs: ["+city_url+"]");
-    locator.setDataURLs(trie_url, city_url);
-    locator.fetchTrie();
+    getLocator().setDataURLs(trie_url, city_url);
+    getLocator().fetchTrie();
 }
 
 // Register an update listener
 function onPrefChange(prefName) {
     console.log("The preference " + prefName + " value has changed!");
     console.log("New pref value: [" + simple_prefs.prefs[prefName] + "]");
-    update_city_urls(locator);
+    update_city_urls();
 }
-update_city_urls(locator);
+update_city_urls();
 simple_prefs.on("offlineCity", onPrefChange);
 
 var page = pageMod.PageMod({
@@ -43,11 +48,11 @@ var page = pageMod.PageMod({
                       // Note: the trielookup needs to return an
                       // object for *each* worker and the lookup must
                       // operate scoped to just that worker.
-                      locator.set_worker(worker);
+                      getLocator().set_worker(worker);
 
                       console.log("Addon received message: ["+addonMessage+"]");
                       if (addonMessage == "startOfflineScan") {
-                          locator.startWatch();
+                          getLocator().startWatch();
                       }
                   });
               }
