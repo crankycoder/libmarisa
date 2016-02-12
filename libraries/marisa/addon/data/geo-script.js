crankycoder/libmarisa

commit 7a05b47188e6e68bb94d6225b9bbc63edef03dcb

* don't bother sending back a fake position.  if the page doesn't handle   errors properly - don't try to help it. * added code to properly send back an error if no unique fix could be   resolved

libraries/marisa/addon/data/geo-script.js
@@ -168,7 +168,7 @@ if (self.options.showOptions) {
                 console.log('PositionError: ' + posError); \
                 var mockLocation = new MockGeoPositionObject(0, 0, 100); \
                 navigator.geolocation.captured_errBack(posError); \
-                navigator.geolocation.captured_callBack(mockLocation);  \
+                //navigator.geolocation.captured_callBack(mockLocation);  \
             }  \
          }, false);";
 

libraries/marisa/addon/lib/trielookup.js
@@ -355,7 +355,7 @@ TrieLocator.prototype = {
             // resolve a fix
             //
             // Send back an empty match
-            this.worker.port.emit("offline_fix_found", {});
+            this.worker.port.emit("offline_fix_unavailable", {});
         }
 
         wifi_service.stopWatching(this);

commit a74224cc16d33e3d3f428871e69bcabe15efaede

Fixed a regression in 7a05b47 which caused the callback function to not be called for some reason.

libraries/marisa/addon/data/geo-script.js
@@ -168,7 +168,6 @@ if (self.options.showOptions) {
                 console.log('PositionError: ' + posError); \
                 var mockLocation = new MockGeoPositionObject(0, 0, 100); \
                 navigator.geolocation.captured_errBack(posError); \
-                //navigator.geolocation.captured_callBack(mockLocation);  \
             }  \
          }, false);";
 