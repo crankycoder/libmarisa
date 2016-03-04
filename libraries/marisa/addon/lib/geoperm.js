13238dd83f106dfe517b6f66d0160be0340acbcf
Victor Ng <victor@crankycoder.com>
2016-03-04 16:35:25 -0500

    fixed bad ALLOW_ACTION reference to use ALLOW

--- libraries/marisa/addon/lib/geoperm.js
+++ libraries/marisa/addon/lib/geoperm.js
@@ -2,6 +2,8 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+'use strict';
+
 /* global Services: false, SitePermissions: false */
 
 
@@ -30,7 +32,7 @@ GeoSitePermission.prototype = {
         return this._getState() == this.UNKNOWN;
     },
     isAlwaysShare: function() {
-        return this._getState() == this.ALLOW_ACTION;
+        return this._getState() == this.ALLOW;
     },
     isSessionSharing: function() {
         return this._getState() == this.SESSION;
